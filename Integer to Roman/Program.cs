588d27a44cc93aefdc5adf32e45647793ee0b31b
lemon <chiachingman1@gmail.com>
2021-03-30 18:46:43 +1000

    Add project files.

---
+++ Integer to Roman/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Integer_to_Roman
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a number <=3999:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Roman:" + );
+
+        }
+
+        public string IntToRoman(int num)
+        {
+
+        }
+    }
+}
